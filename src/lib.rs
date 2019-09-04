abbff7c503a2ba003decab4e0e94f593d64fc839
Raymond <extraymond@gmail.com>
2019-09-05 02:32:59 +0800

    add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@
 
 pub mod component;
 pub mod utils;
+pub mod prelude {
+    pub use dodrio::Node;
+    pub use dodrio::RenderContext;
+    pub use typed_html::dodrio;
+    pub use wasm_bindgen_futures::futures_0_3::spawn_local;
+}
