3a92ab50432f43e33e127ca98c20d730cb79dac6
Raymond <extraymond@gmail.com>
2019-09-05 02:38:28 +0800

    fix prelude

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,9 @@ pub mod utils;
 pub mod prelude {
     pub use dodrio::Node;
     pub use dodrio::RenderContext;
-    pub use futures::{
-        channel::mpsc,
-        future::{AbortHandle, Abortable},
-        sink::SinkExt,
-    };
+    pub use futures::sink::SinkExt;
     pub use typed_html::dodrio;
-    pub use wasm_bindgen::prelude::*;
+    pub use wasm_bindgen::JsCast;
     pub use wasm_bindgen_futures::futures_0_3::spawn_local;
     pub use web_sys::Event;
 }
