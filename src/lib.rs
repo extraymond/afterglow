5d0513b264e1993261ff34ad9c008afc96dd98b9
Raymond <extraymond@gmail.com>
2019-09-05 20:40:50 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod prelude {
     pub use dodrio::Node;
     pub use dodrio::RenderContext;
     pub use futures::sink::SinkExt;
-    pub use typed_html::dodrio;
+    pub use typed_html::{self, dodrio};
     pub use wasm_bindgen::JsCast;
     pub use wasm_bindgen_futures::futures_0_3::spawn_local;
     pub use web_sys::Event;
