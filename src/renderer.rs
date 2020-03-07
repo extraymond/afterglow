d122cc22e9c8d8689cd100c30538699a1222e4b1
Raymond Yeh <extraymond@gmail.com>
2020-03-07 15:41:46 +0800

    yes

--- src/renderer.rs
+++ src/renderer.rs
@@ -23,17 +23,6 @@ impl<'a, T> dodrio::Render<'a> for Container<T> {
     }
 }
 
-impl<T> Container<T> {
-    pub fn render<'a>(&self, ctx: &mut RenderContext<'a>) -> Node<'a> {
-        let bump = ctx.bump;
-        if let Some(data) = self.data.try_lock() {
-            self.renderer.view(&*data, ctx, self.sender.clone())
-        } else {
-            dodrio!(bump, <template></template>)
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
