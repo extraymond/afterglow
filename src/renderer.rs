d42765a725270539dae8e276787ae0a9d2cc4dd8
Raymond Yeh <extraymond@gmail.com>
2020-03-07 15:55:09 +0800

    include context renderder.

--- src/renderer.rs
+++ src/renderer.rs
@@ -12,6 +12,29 @@ pub trait Renderer {
     ) -> Node<'a>;
 }
 
+pub trait ContextRenderer<A> {}
+
+impl<A, T> ContextRenderer<A> for T where T: Renderer<Target = A, Data = A> {}
+
+impl<T> Renderer for dyn ContextRenderer<T> {
+    type Target = Container<T>;
+    type Data = Container<T>;
+
+    fn view<'a>(
+        &self,
+        target: &Self::Target,
+        ctx: &mut RenderContext<'a>,
+        sender: Sender<Box<dyn Messenger<Target = Self::Data>>>,
+    ) -> Node<'a> {
+        let bump = ctx.bump;
+        if let Some(data) = target.data.try_lock() {
+            target.renderer.view(&*data, ctx, target.sender.clone())
+        } else {
+            dodrio!(bump, <template></template>)
+        }
+    }
+}
+
 impl<'a, T> dodrio::Render<'a> for Container<T> {
     fn render(&self, cx: &mut RenderContext<'a>) -> Node<'a> {
         let bump = cx.bump;
